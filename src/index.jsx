005ca80bd58ff18f289d6ae33a98df0e8443a2ef
Clayton1984 <clayton.albano@superior-ind.com>
2022-03-21 16:53:15 -0300

    Organizando parte1

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '././templates/index.css';
+import App from './templates/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
